cfa0b532b7d2b150382c1e8e9edd9d7ec5ef27f7
alex <alejandro.gonzalez@fusuma.io>
2022-08-02 15:40:39 +0200

    added splash screen dependency and icons images

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:bmatch/injection.dart';
 import 'package:bmatch/presentation/app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 Future<void> startApp(Environm env) async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const AppWidget());
+
+  FlutterNativeSplash.remove();
 }
+
