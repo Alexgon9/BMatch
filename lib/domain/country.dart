3f70327b0400a4e95217e19de000f545124812b2
Alexgon9 <alejandro.gonzalez@fusuma.io>
2022-08-03 11:58:04 +0200

    created country selection page and country class

---
+++ lib/domain/country.dart
@@ -0,0 +1,11 @@
+class Country {
+  final String countryName;
+  final String countryFlag;
+
+  Country(
+      this.countryName,
+      this.countryFlag
+  );
+}
+
+
--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -6,8 +6,58 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BMatch'),
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 50, bottom: 50),
+              child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: Image.asset('assets/icon/LogoBmatch.png')),
+            ),
+            const Text('Por favor, selecciona un lugar de residencia'),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.4,
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: GridView(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 10,
+                    childAspectRatio: 2
+                  ),
+                  children: [
+                    _buildCountryButton('Mexico'),
+                    _buildCountryButton('Mexico'),
+                    _buildCountryButton('Mexico'),
+                    _buildCountryButton('Mexico')
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCountryButton(String country) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black, width: 1),
+          borderRadius: const BorderRadius.all(Radius.circular(25))
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(country),
+          ],
+        ),
       ),
     );
   }
